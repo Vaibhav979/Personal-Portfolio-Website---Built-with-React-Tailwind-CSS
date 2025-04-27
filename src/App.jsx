c361601adb590c1f1960d5f3f19e4db2cb74b6a2
vaibhav <vaibhavsingh8931@gmail.com>
2025-04-28 00:13:26 +0530

    added home section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import LoadingScreen from "./components/LoadingScreen";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import MobileMenu from "./components/MobileMenu";
+import Home from "./components/sections/Home";
+import About from "./components/sections/About";
+import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -19,6 +23,8 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
       </div>
     </>
   );
