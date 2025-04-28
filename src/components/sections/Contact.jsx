8be28c67b3d8ba40989b7cdea556aaccd9823854
vaibhav <vaibhavsingh8931@gmail.com>
2025-04-28 12:03:15 +0530

    added contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,7 +1,103 @@
 import React from "react";
+import RevealOnScroll from "../RevealOnScroll";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_serviceId,
+        import.meta.env.VITE_TemplateId,
+        e.target,
+        import.meta.env.VITE_publicKey
+      )
+      .then((result) => {
+        alert("Message Sent!");
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      })
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
+  };
+
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="px-4 w-150">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            Get In Touch{" "}
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="relative">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+                className="w-full bg-white/5 border hover:-translate-y-1 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Name..."
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                className="w-full bg-white/5 border hover:-translate-y-1 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="example@gamil.com"
+              />
+            </div>
+
+            <div className="relative">
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+                required
+                rows={5}
+                className="w-full bg-white/5 border hover:-translate-y-1 border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                placeholder="Your message..."
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)"
+            >
+              {" "}
+              Send Message{" "}
+            </button>
+          </form>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
 };
 
 export default Contact;
