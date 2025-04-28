b64f4ae5dafb474d7b671d5279d402577dd20cf0
vaibhav <vaibhavsingh8931@gmail.com>
2025-04-28 17:42:42 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/Personal-Portfolio-Website---Built-with-React-Tailwind-CSS"
 });
